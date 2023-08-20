0d4eb7057c83902805afe7203fc492c080ef7e66
Egorikms <epomidorka10@gmail.com>
2023-08-20 20:05:10 +0300

    ItWorks

--- parser1.py
+++ parser1.py
@@ -1,28 +1,26 @@
-import asyncio
-import aiohttp
-
+import requests
 from bs4 import BeautifulSoup as BS
-from fake_useragent import UserAgent
 
-BASE_URL = "https://www.globus.ru/restaurant/" 
-HEADERS = {"User-Agent": UserAgent().random}
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(BASE_URL, headers=HEADERS) as response: 
-            r = await aiohttp.StreamReader.read(response.content)
-            soup = BS(r, "html.parser")
-            items = soup.find_all("a", {"class": "pim-list__item "}) 
-            
-            for item in items:
-                title = item.find("div", {"class": "pim-list__item-title js-crop-text"})
-                link = title.get("href")
-                price = item.find("span", "pim-list__item-price-actual-main").text.strip()
-                print(f"{title.text.strip()} | {price} | https://www.globus.ru{link}")
-                print()
+def main():
+    
+    url = "https://kino-format.ru/films/"
+    r = requests.get(url)
+    soup = BS(r.text, "html.parser")
+    items = soup.find_all("div", {"class": "col-lg-2 col-md-3 col-sm-4 col-6"})
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop() 
-    loop.run_until_complete(main())
+    for item in items:
+        name = item.find("h3", {"class": "film-name"}).text.strip()
+        janr = item.find("p", {"class": "film-genre"}).text.strip()
+        age = item.find("p", {"class": "adult-marker"}).text.strip()
+        print(f"{name} | {janr} | {age}")
+        print()
+
+
+main()
+
+
+
+
 
 
